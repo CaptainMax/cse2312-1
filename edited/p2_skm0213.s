91a318a629f806dab9eb75105ee6ac8aef3549a9
Satej Mhatre <pi@raspberry.com>
2016-11-20 20:02:00 -0600

    FINISHEDnano p2_skm0213.s !

--- edited/p2_skm0213.s
+++ edited/p2_skm0213.s
@@ -16,26 +16,30 @@ main:
     MOV R1, R0
     BL  _getint
     MOV R2, R0              @ copy return value to R1
+    PUSH {R1}
+    PUSH {R2}
     BL _gcd
+    POP {R2}
+    POP {R1}
     MOV R3, R0
-	BL  _print_val          @ print value stored in R1
-	B   main                @ loop to main procedure with no return
+    BL  _print_val          @ print value stored in R1
+    B   main                @ loop to main procedure with no return
 
 _gcd:
     PUSH {LR}               @ store the return address
-    CMP R1, #0              @ compare the input argument to 1
-    MOVEQ R0, #1            @ set return value to 1 if equal
+    CMP R2, #0              @ compare the input argument to 1
+    MOVEQ R0, R1            @ set return value to 1 if equal
     POPEQ {PC}              @ restore stack pointer and return if equal
 
     PUSH {R1}               @ backup input argument value
     PUSH {R2}               @ backup input argument value
-    MOV R1, R2
     BL _mod_unsigned
+    MOV R1, R2
     MOV R2, R0
 
     BL _gcd                 @ compute gcd with new args
-    POP {R1}
-    POP {R2}                @ restore input argument
+    POP {R2}
+    POP {R1}                @ restore input argument
     POP {PC}               @ restore the stack pointer and return
 _mod_unsigned:
     cmp R2, R1              @ check to see if R1 >= R2
@@ -54,17 +58,20 @@ _mod_unsigned:
     MOV R0, R1              @ move remainder to R0
     MOV PC, LR              @ return
 
-
 _getint:
     PUSH {LR}               @ store LR since scanf call overwrites
+    PUSH {R1}
+    PUSH {R2}
     SUB SP, SP, #4          @ make room on stack
     LDR R0, =input          @ R0 contains address of format string
     MOV R1, SP              @ move SP to R1 to store entry on stack
     BL scanf                @ call scanf
     LDR R0, [SP]            @ load value at SP into R0
     ADD SP, SP, #4          @ restore the stack pointer
+    POP {R2}
+    POP {R1}
     POP {PC}                @ return
-   
+
 _print_val:
 	MOV R4, LR              @ store LR since printf call overwrites
 	LDR R0,=result_str      @ string at label hello_str:
