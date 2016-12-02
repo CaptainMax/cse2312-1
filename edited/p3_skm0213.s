8c5766b6c718558b77c967aa4b3b2806a8a4aeaa
satej1210 <satej1210@live.com>
2016-12-01 20:09:26 -0600

    and

--- edited/p3_skm0213.s
+++ edited/p3_skm0213.s
@@ -15,8 +15,6 @@
 .func main
 
 main:
-    MOV R5, #0
-    MOV R6, #0
     BL _seedrand
     BL _arrayMake
 
@@ -53,11 +51,8 @@ writeloop:
     PUSH {R0}               @ backup iterator before procedure call
     PUSH {R2}               @ backup element address before procedure call
     BL _getrand             @ get a random number
-    CMP R1, #0
-    BEQ _setMinMax
-    BL _changeMinMax
-    POP {R2}                @ restore element address
 
+    POP {R2}                @ restore element address
     STR R0, [R2]            @ write the address of a[i] to a[i]
     POP {R0}                @ restore iterator
     ADD R0, R0, #1          @ increment index
@@ -76,7 +71,9 @@ readloop:
     PUSH {R2}               @ backup register before printf
     MOV R2, R1              @ move array value to R2 for printf
     MOV R1, R0              @ move array index to R1 for printf
-
+CMP R0, #0
+BEQ _setMinMax
+BL _changeMinMax
     BL  _printf             @ branch to print procedure with return
     POP {R2}                @ restore register
     POP {R1}                @ restore register
