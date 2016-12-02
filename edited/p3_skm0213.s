76bb52eebe6f53a8e02534a8fb967d0100c24fb5
satej1210 <satej1210@live.com>
2016-12-01 20:21:15 -0600

    salads

--- edited/p3_skm0213.s
+++ edited/p3_skm0213.s
@@ -127,6 +127,21 @@ _getrand:
     MOV R2, #1000
     BL _mod_unsigned
     POP {PC}                @ return
+_mod_unsigned:
+    cmp R2, R1          @ check to see if R1 >= R2
+    MOVHS R0, R1        @ swap R1 and R2 if R2 > R1
+    MOVHS R1, R2        @ swap R1 and R2 if R2 > R1
+    MOVHS R2, R0        @ swap R1 and R2 if R2 > R1
+    MOV R0, #0          @ initialize return value
+    B _modloopcheck     @ check to see if
+    _modloop:
+    ADD R0, R0, #1  @ increment R0
+    SUB R1, R1, R2  @ subtract R2 from R1
+    _modloopcheck:
+    CMP R1, R2      @ check for loop termination
+    BHS _modloop    @ continue loop if R1 >= R2
+    MOV R0, R1          @ move remainder to R0
+    MOV PC, LR          @ return
 
 .data
 
